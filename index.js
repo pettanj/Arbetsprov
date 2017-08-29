55cb527c8873b22dee35003b06508728289c8eaf
petrus <petrus.jonsson@gmail.com>
2017-08-29 22:11:34 +0200

    Add auto-complete, save and styles

--- index.js
+++ index.js
@@ -1,30 +1,70 @@
-var temp = "hello";
-var url = "https://restcountries.eu/rest/v2/name/";
-function submit() {
-    document.getElementById("result-table").setAttribute("style", "display: none;")
-    name = document.getElementById("search-input").value;
-    fetch(url + name).then(function (response) {
-        return response.json();
-    }).then(function (value) {
-        document.getElementById("result-table").getElementsByTagName('tbody')[0].innerHTML = "";
-        value.forEach(function (item) {
-            var tr = document.createElement("TR");
-            var nameColumn = document.createElement("TD");
-            nameColumn.innerHTML = item.name;
-            var timeColumn = document.createElement("TD");
-            timeColumn.innerHTML = new Date().toLocaleDateString("sv-se");
-            tr.appendChild(nameColumn);
-            tr.appendChild(timeColumn);
-            document.getElementById("result-table").getElementsByTagName('tbody')[0].appendChild(tr);
-        })
-        document.getElementById("result-table").setAttribute("style", "display: block;")
-    }).catch(function (response) {
-        document.getElementById("result-table").setAttribute("style", "display: block;")
-        document.getElementById("result-table").getElementsByTagName('tbody')[0].innerHTML = "No response";
+var url = "https://restcountries.eu/rest/v2/name/",
+    partialResult = [],
+    resultHistory = [],
+    $search = document.getElementById("search-input"),
+    $results = document.getElementById("result-table");
+
+
+var ac = new autoComplete({
+    selector: '#search-input',
+    minChars: 1,
+    source: function (country, response) {
+        getCountries(country.toLowerCase()).then(function(data) {
+            response(data);
+        });
+    },
+    onSelect: function (e, country, item) {
+        saveItem(country);
+    }
+});
+
+function getCountries(country) {
+    return fetch(url + country).then(function (res) {
+        return res.json();
+    }).then(function (data) {
+        return data.map(function (country) {
+            console.log(country);
+            return country.name;
+        });
     });
 }
-function keypressFunction(event) {
-    if (event.keyCode == 13) {
-        submit();
-    }
+
+function removeItem(item, node) {
+    resultHistory.splice(resultHistory.indexOf(item), 1);
+    $results.getElementsByTagName('tbody')[0].removeChild(node);
+}
+
+function addName(tr, country) {
+    var column = document.createElement("TD");
+    column.innerHTML = country;
+    tr.appendChild(column);
+}
+
+function addTimeStamp(tr) {
+    var options = { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+    var column = document.createElement("TD");
+    column.innerHTML = new Date().toLocaleDateString("sv-se", options);
+    tr.appendChild(column);
+}
+
+function addDelete(tr, country) {
+    var column = document.createElement("TD");
+    column.setAttribute("class", "delete-column");
+    var deleteBtn = document.createElement("BUTTON");
+    deleteBtn.setAttribute("class", "delete-button")
+    deleteBtn.innerHTML = "X";
+    deleteBtn.onclick = function () { removeItem(country, tr) };
+    column.appendChild(deleteBtn);
+    tr.appendChild(column);
+}
+
+function saveItem(country) {
+    $results.setAttribute("style", "display: table;")
+    var tr = document.createElement("TR");
+    addName(tr, country);
+    addTimeStamp(tr);
+    addDelete(tr, country);
+    resultHistory.push(country);
+    $results.getElementsByTagName('tbody')[0].appendChild(tr);
+    $search.value = "";
 }
